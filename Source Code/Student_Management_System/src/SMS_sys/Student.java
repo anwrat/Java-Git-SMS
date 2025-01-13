3471e83d4fe23385466110683e6129e17f662aad
anwrat <anweshrwt22@gmail.com>
2025-01-13 11:08:22 +0545

    Added getter setter methods for student.java

--- Source Code/Student_Management_System/src/SMS_sys/Student.java
+++ Source Code/Student_Management_System/src/SMS_sys/Student.java
@@ -1,10 +1,21 @@
 package SMS_sys;
 
 public class Student {
-	String name;
-	int age;
-	public Student(String name,int age) {
-		this.name=name;
+	private String  name;
+	private int age;
+	public Student() {
+		
+	}
+	public void setAge(int age) {
 		this.age=age;
+	}
+	public void setName(String name) {
+		this.name=name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public String getName() {
+		return name;
 	}
 }
